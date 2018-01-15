eda31d37414732d5a27399e4e69ad8b01ecc382d
Sev Leonard <jennifer.m.leonard@gmail.com>
2018-01-14 16:41:19 -0800

    more slide updates, adding some supporting code files

---
+++ boot.py
@@ -0,0 +1,9 @@
+import gc
+import webrepl
+webrepl.start()
+gc.collect()
+import dht
+import machine
+gc.collect()
+from mqtt_measure import *
+gc.collect()
---
+++ mqtt_measure.py
@@ -0,0 +1,60 @@
+import dht
+import time
+import machine
+import gc
+gc.collect()
+from umqtt.simple import MQTTClient
+gc.collect()
+my_dht = dht.DHT11(machine.Pin(2))
+
+def measure(poll_time_s):
+    while True:
+        humidity = 51
+        temp = 13
+        try:
+            my_dht.measure()
+            humidity = my_dht.humidity()
+            temp = my_dht.temperature()
+        except Exception as ex:
+            print('exception in measure function' + str(ex))
+        print("humidity: ", humidity)
+        print("temp: ", temp)
+        gc.collect()
+        time.sleep(poll_time_s)
+
+def measure_mqtt(poll_time_s, broker_ip, client_id, topic):
+    p = MQTTClient(client_id, broker_ip)
+    try :
+        p.connect()
+    except Exception as ex:
+        print('exception in measure_mqtt trying to connect to broker ' + str(ex))
+
+    while True:
+        humidity = 51
+        temp = 13
+        try:
+            my_dht.measure()
+            humidity = my_dht.humidity()
+            temp = my_dht.temperature()
+        except Exception as ex:
+            print('exception in measure function' + str(ex))
+
+        gc.collect()
+        temp_topic = topic + '/temp/' + client_id
+        humid_topic =  topic + '/humidity/' + client_id
+        print('publishing to: ' + temp_topic + '  value: ' + str(temp))
+        print('publishing to: ' + humid_topic + '  value: ' + str(humidity))
+
+        try :
+            p.publish(temp_topic, str(temp))
+            p.publish(humid_topic, str(humidity))
+        except Exception as ex:
+            print('exception in measure_mqtt publish: ' + str(ex))
+            print('retrying connect')
+            try :
+                p.connect()
+            except Exception as ex:
+                print('exception in measure_mqtt trying to connect to broker ' + str(ex))
+
+        gc.collect()
+        time.sleep(poll_time_s)
